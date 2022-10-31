05439c4d6d5ed173a12ca3d67b1b56c3620d432d
floroldos <florencia.roldos@correo.ucu.edu.uy>
2022-10-31 09:14:16 -0300

    Agregados coraza y palo

---
+++ src/Library/characters/Trolls.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using RoleplayGame.Items;
+
+namespace RoleplayGame.Characters
+{
+    /// <summary>
+    /// Personaje Mago
+    /// </summary>
+    public class Trolls: Character
+    {
+        public Trolls(string name)
+            : base(name)
+        {
+            this.Health = 80;
+            this.AddItem(new Magic());
+        }
+    }
+}
---
+++ src/Library/items/Coraza.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace RoleplayGame.Items
+{
+    public class Coraza : IDefenseItem
+    {
+        public int DefensePower
+        {
+            get
+            {
+                return 18;
+            }
+        }
+    }
+}
