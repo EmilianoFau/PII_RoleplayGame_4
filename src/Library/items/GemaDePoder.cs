63258bfe58872cbce75ed90964b173098fb4827c
EmilianoFau <emiliano.fau@correo.ucu.edu.uy>
2022-10-31 09:18:17 -0300

    Baston Magico agregado utilizando composicion

---
+++ src/Library/items/GemaDePoder.cs
@@ -0,0 +1,13 @@
+using System;
+namespace RoleplayGame.Items;
+
+public class GemaDePoder : IAttackItem
+{
+    public int AttackPower
+    {
+        get
+        {
+            return 10;
+        }
+    }
+}
