892a522feb6a6cc3f3c531772fc6e24841210aa2
Renzo Giacometti <renzo.giacometti@gmail.com>
2022-10-31 09:23:18 -0300

    Escudo Mágico terminado

--- src/Library/items/EscudoMagico.cs
+++ src/Library/items/EscudoMagico.cs
@@ -4,13 +4,19 @@ namespace RoleplayGame.Items
 {
     public class EscudoMagico : IDefenseItem
     {
-        EscudoMedieval escudoMedieval = new EscudoMedieval();
-        GemaMagicaDefensa gemaMagicaDefensa = new GemaMagicaDefensa();
+        private EscudoMedieval EscudoMedieval {get; set;}
+        private GemaMagicaDefensa GemaMagicaDefensa {get; set;}
+
+        public EscudoMagico(EscudoMedieval escudoMedieval, GemaMagicaDefensa gemaMagicaDefensa)
+        {
+            this.GemaMagicaDefensa = gemaMagicaDefensa;
+            this.EscudoMedieval = escudoMedieval;
+        }
         public int DefensePower
         {
             get
             {
-                return escudoMedieval.DefensePower + gemaMagicaDefensa.DefensePower;
+                return EscudoMedieval.DefensePower + GemaMagicaDefensa.DefensePower;
             }
         }
     }
