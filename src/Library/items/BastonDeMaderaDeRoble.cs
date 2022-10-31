cd690ca7c9bcefb074c58ac5b850d22f8c04d190
EmilianoFau <emiliano.fau@correo.ucu.edu.uy>
2022-10-31 08:44:10 -0300

    Linking issue

---
+++ src/Library/items/BastonDeMaderaDeRoble.cs
@@ -0,0 +1,13 @@
+using System;
+namespace RoleplayGame.Items;
+
+public class BastonDeMaderaDeRoble : IAttackItem
+{
+    public int AttackPower
+    {
+        get
+        {
+            return 60;
+        }
+    }
+} 
